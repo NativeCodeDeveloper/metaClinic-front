00ad1b7d6b90a230a4f17399a6bbc98debeef40d
NativeCode <tm.nicolasmachuca@gmail.com>
2026-06-06 10:47:17 -0400

    clerk

--- src/lib/clerk-disabled.js
+++ src/lib/clerk-disabled.js
@@ -1,4 +1,4 @@
-export const CLERK_TEMPORARILY_DISABLED = true;
+export const CLERK_TEMPORARILY_DISABLED = false;
 
 export function isClerkTemporarilyDisabled() {
   return CLERK_TEMPORARILY_DISABLED;
